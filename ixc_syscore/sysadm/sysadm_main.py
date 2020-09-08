76f733aa8b75293c39c0b5e1da6a3c893345e6e9
kaimei <freekai@outlook.com>
2020-09-08 18:37:35 +0800

    ...

--- ixc_syscore/sysadm/sysadm_main.py
+++ ixc_syscore/sysadm/sysadm_main.py
@@ -7,6 +7,7 @@ if not os.path.isdir(os.getenv("IXC_MYAPP_TMP_DIR")): os.mkdir(os.getenv("IXC_MY
 
 import pywind.evtframework.evt_dispatcher as dispatcher
 import pywind.lib.proc as proc
+import pywind.lib.configfile as cfg
 
 import ixc_syslib.pylib.logging as logging
 
@@ -14,11 +15,13 @@ import ixc_syscore.sysadm.handlers.httpd as httpd
 
 PID_FILE = "%s/proc.pid" % os.getenv("IXC_MYAPP_TMP_DIR")
 
+
 def __stop_service():
     pid = proc.get_pid(PID_FILE)
     if pid < 0: return
 
     os.kill(pid, signal.SIGINT)
+
 
 def __start_service(debug):
     if not debug and os.path.isfile(PID_FILE):
@@ -60,11 +63,17 @@ class service(dispatcher.dispatcher):
 
     __debug = None
 
+    __httpd_cfg_path = None
+    __httpd_configs = None
+
     def load_configs(self):
-        pass
+        self.__httpd_configs = cfg.ini_parse_from_file(self.__httpd_cfg_path)
 
     def write_to_configs(self):
         pass
+
+    def service_start(self):
+        listen = self.__httpd_configs["listen"]
 
     def init_func(self, debug):
         self.__httpd_fd = -1
@@ -74,7 +83,12 @@ class service(dispatcher.dispatcher):
 
         self.__debug = debug
 
+        self.__httpd_cfg_path = "%s/httpd.ini" % os.getenv("IXC_MYAPP_CONF_DIR")
+
+        self.load_configs()
         self.create_poll()
+
+        self.service_start()
 
     @property
     def debug(self):
@@ -82,3 +96,30 @@ class service(dispatcher.dispatcher):
 
     def release(self):
         pass
+
+
+def main():
+    __helper = "ixc_syscore/sysadm helper: start | stop | debug"
+
+    if len(sys.argv) != 2:
+        print(__helper)
+        return
+
+    action = sys.argv[1]
+    if action not in ("start", "stop", "debug",):
+        print(__helper)
+        return
+
+    if action == "stop":
+        __stop_service()
+        return
+
+    if action == "debug":
+        debug = True
+    else:
+        debug = False
+
+    __start_service(debug)
+
+
+if __name__ == '__main__': main()
