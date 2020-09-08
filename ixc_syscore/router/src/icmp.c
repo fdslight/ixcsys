465eb20d791ee6f4439ff512eaf1ce1ac09a792a
kaimei <freekai@outlook.com>
2020-09-08 18:23:05 +0800

    ...

--- ixc_syscore/router/src/icmp.c
+++ ixc_syscore/router/src/icmp.c
@@ -12,6 +12,7 @@ static void ixc_icmp_handle_self(struct ixc_mbuf *m)
     int hdr_len= (header->ver_and_ihl & 0x0f) * 4;
     struct netutil_icmphdr *icmphdr=(struct netutil_icmphdr *)(m->data+m->offset+hdr_len);
     unsigned char src_ipaddr[4],dst_ipaddr[4];
+    unsigned short csum;
 
     // 只处理echo请求
     if(8!=icmphdr->type && 0!=icmphdr->code){
@@ -22,11 +23,11 @@ static void ixc_icmp_handle_self(struct ixc_mbuf *m)
     memcpy(src_ipaddr,header->dst_addr,4);
     memcpy(dst_ipaddr,header->src_addr,4);
 
-    STDERR("%d.%d.%d.%d\r\n",dst_ipaddr[0],dst_ipaddr[1],dst_ipaddr[2],dst_ipaddr[3]);
-
     rewrite_ip_addr(header,src_ipaddr,1);
     rewrite_ip_addr(header,dst_ipaddr,0);
 
+    csum=csum_calc_incre(icmphdr->type,0,icmphdr->checksum);
+    icmphdr->checksum=csum;
     icmphdr->type=0;
 
     ixc_ip_send(m);
