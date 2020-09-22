b5fb61727e79eb0e6fdf8fb1b2996dda6796ee98
kaimei <freekai@outlook.com>
2020-09-22 20:50:41 +0800

    ...

--- ixc_syscore/DHCP/dhcplib/dhcp.py
+++ ixc_syscore/DHCP/dhcplib/dhcp.py
@@ -1,4 +1,163 @@
 #!/usr/bin/env python3
 
-class dhcp_header(object):
-    pass
+import struct, random
+
+DHCP_OP_REQ = 1
+DHCP_OP_RESP = 2
+
+DHCP_OPS = [
+    DHCP_OP_REQ, DHCP_OP_RESP
+]
+
+
+class DHCPErr(Exception): pass
+
+
+HDR_FMT = "!BBBBIHH4s4s4s4s16s64s128sI"
+
+HDR_LENGTH = 240
+
+
+class dhcp(object):
+    op = None
+    hw_type = None
+    hw_len = None
+    h_ops = None
+    xid = None
+    secs = None
+    flags = None
+
+    ciaddr = None
+    yiaddr = None
+    siaddr = None
+    giaddr = None
+    chaddr = None
+
+    sname = None
+    file = None
+
+    __magic_cookie = None
+
+    def __init__(self):
+        self.op = DHCP_OP_REQ
+        self.hw_type = 1
+        self.hw_len = 6
+        self.h_ops = 0
+        self.xid = random.randint(1, 0xfffffffe)
+        self.secs = 0
+        self.flags = 0
+
+        self.ciaddr = bytes(4)
+        self.yiaddr = bytes(4)
+        self.siaddr = bytes(4)
+        self.giaddr = bytes(4)
+
+        self.chaddr = bytes(16)
+
+        self.sname = bytes(64)
+        self.file = bytes(128)
+
+        self.__magic_cookie = 0x63825363
+
+        self.my_init()
+
+    def my_init(self):
+        """重写这个方法
+        """
+        pass
+
+    @property
+    def magic_cookie(self):
+        return self.__magic_cookie
+
+
+class dhcp_parser(dhcp):
+    def parse_options(self, byte_data: bytes):
+        """解析DHCP options
+        """
+        results = []
+        i = 0
+
+        while 1:
+            try:
+                code = byte_data[i]
+            except IndexError:
+                raise DHCPErr("wrong DHCP packet")
+            if 0 == code: continue
+            if code == 0xff: break
+            i += 1
+            try:
+                length = byte_data[i]
+            except IndexError:
+                raise DHCPErr("wrong DHCP packet")
+            a = i + 1
+            b = a + length
+
+            data = byte_data[a:b]
+
+            if len(data) != length:
+                raise DHCPErr("wrong DHCP option length")
+            results.append((code, length, data,))
+            i = b
+        return results
+
+    def parse_public_header(self, public_data: bytes):
+        try:
+            self.op, self.hw_type, self.hw_len, self.h_ops, self.xid, self.secs, self.flags, \
+            self.ciaddr, self.yiaddr, self.siaddr, self.giaddr, \
+            self.chaddr, self.sname, self.file, magic_cookie = struct.unpack(HDR_FMT, public_data)
+        except struct.error:
+            raise DHCPErr("wrong DHCP data")
+
+        if magic_cookie != self.magic_cookie:
+            raise DHCPErr("wrong DHCP magic cookie")
+
+    def my_init(self):
+        pass
+
+
+class dhcp_builder(dhcp):
+    def my_init(self):
+        pass
+
+    def build(self, options: list):
+        """options 格式为 [(code,byte_value),...]
+        """
+        try:
+            pub_data = struct.pack(HDR_FMT,
+                                   self.op,
+                                   self.hw_type,
+                                   self.hw_len,
+                                   self.h_ops,
+                                   self.xid,
+                                   self.secs,
+                                   self.flags,
+                                   self.ciaddr,
+                                   self.yiaddr,
+                                   self.siaddr,
+                                   self.giaddr,
+                                   self.chaddr,
+                                   self.sname,
+                                   self.file,
+                                   self.magic_cookie
+                                   )
+        except struct.error:
+            raise DHCPErr("wrong DHCP data format")
+
+        _list = []
+        for code, byte_value in options:
+            length = len(byte_value)
+
+            try:
+                x = struct.pack("!BB", code, length)
+            except struct.error:
+                raise DHCPErr("wrong option value")
+
+            _list.append(x)
+            _list.append(byte_value)
+
+        _list.append(struct.pack("!I", self.magic_cookie))
+
+        opt_data = b"".join(_list)
+
+        return b"".join([pub_data, opt_data, ])
---
+++ ixc_syscore/DHCP/dhcplib/tcpip.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""TCP/IP构建以及解析
+"""
+
+import struct, random
+
+
+class TCPIPErr(Exception): pass
+
+
+def csum_calc(packet: bytes):
+    """计算csum
+    """
+    size = len(packet)
+    checksum = 0
+    a = 0
+    b = 1
+    while size > 1:
+        checksum += (packet[a] << 8) | packet[b]
+        size -= 2
+        a += 2
+        b += 2
+
+    if size:
+        checksum += packet[a]
+
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum += (checksum >> 16)
+
+    return (~checksum) & 0xffff
+
+
+def build_ether_data(dst_hwaddr: bytes, src_hwaddr: bytes, link_proto: int, byte_data: bytes):
+    """构建以太网数据包
+    """
+    header_data = struct.pack("!6s6sH", dst_hwaddr, src_hwaddr, link_proto)
+
+    return b"".join([header_data, byte_data])
+
+
+def parse_ether_data(byte_data: bytes):
+    """解析以太网数据包
+    """
+    if len(byte_data) < 14:
+        raise TCPIPErr("wrong ethernet packet")
+
+    dst_hwaddr, src_hwaddr, proto = struct.unpack("!6s6sH", byte_data[0:14])
+    r = (dst_hwaddr, src_hwaddr, proto, byte_data[14:],)
+
+    return r
+
+
+def build_udppkt(src_ipaddr: bytes, dst_ipaddr: bytes, src_port: int, dst_port: int, payload_data: bytes,
+                 is_ipv6=False):
+    """构建UDP数据包
+    """
+    length = len(payload_data)
+    header = struct.pack("!HHHH", src_port, dst_port, length, 0)
+    udp_data = b"".join([header, payload_data])
+
+    # 伪头部
+    if not is_ipv6:
+        ps_hdr = struct.pack("!4s4sBBH", src_ipaddr, dst_ipaddr, 0, 17, length)
+    else:
+        ps_hdr = struct.pack("!16s16sBBH", src_ipaddr, dst_ipaddr, 0, 17, length)
+    tmp_data = b"".join([ps_hdr, udp_data])
+
+    csum = csum_calc(tmp_data)
+
+    udp_data[4] = (csum & 0xff00) >> 8
+    udp_data[5] = csum & 0x00ff
+
+    return udp_data
+
+
+def build_ippkt(src_ipaddr: bytes, dst_ipaddr: bytes, protocol: int, payload_data: bytes):
+    """构建IP数据包
+    """
+    header = struct.pack("!BBHHHBBH4s4s",
+                         0x45, 0, len(payload_data),
+                         random.randint(1, 0xfffe), 0b0100_0000_0000_0000_0000_0000_0000_0000,
+                         64, protocol, 0,
+                         src_ipaddr, dst_ipaddr
+                         )
+    csum = csum_calc(header)
+    header[10] = (csum & 0xff00) >> 8
+    header[11] = csum & 0xff
+
+    return b"".join([header, payload_data])
+
+
+def parse_ippkt(byte_data: bytes):
+    size = len(byte_data)
+    hdr_len = (byte_data[0] & 0x00ff) * 4
+
+    if size < hdr_len:
+        raise TCPIPErr("wrong data length")
+
+    payload_data = byte_data[hdr_len:]
+
+    r = (byte_data[12:16], byte_data[16:20], byte_data[9], payload_data,)
+
+    return r
+
+
+def parse_udppkt(udp_data: bytes):
+    if len(udp_data) < 9: return TCPIPErr("wrong UDP packet")
+
+    src_port, dst_port, length, csum = struct.unpack("!HHHH", udp_data[0:8])
+    r = (src_port, dst_port, udp_data[8:],)
+
+    return r
+
+
+def arp_build():
+    """构建ARP数据包
+    """
+    pass
+
+
+def arp_parse(byte_data: bytes):
+    """解析ARP数据包
+    """
+    pass
