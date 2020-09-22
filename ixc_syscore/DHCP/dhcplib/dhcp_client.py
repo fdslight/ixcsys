e623d21da80c91a6ae9acfdb3455dd9a87ffe1fd
kaimei <freekai@outlook.com>
2020-09-22 20:50:41 +0800

    ...

--- ixc_syscore/DHCP/dhcplib/dhcp_client.py
+++ ixc_syscore/DHCP/dhcplib/dhcp_client.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+
+import ixc_syscore.DHCP.dhcplib.dhcp as dhcp
+
+
+class dhcp_client(object):
+    __my_ipaddr = None
+
+    __hostname = None
+    __hwaddr = None
+
+    __dhcp_parser = None
+    __dhcp_builder = None
+
+    __runtime = None
+
+    def __init__(self, runtime, hostname: str, hwaddr: str):
+        self.__runtime = runtime
+        self.__hostname = hostname
+        self.__hwaddr = hwaddr
+
+        self.__dhcp_parser = dhcp.dhcp_parser()
+        self.__dhcp_builder = dhcp.dhcp_builder()
+
+    def handle_dhcp_response(self, response_data: bytes):
+        pass
+
+    def do(self):
+        """自动执行
+        """
+        pass
