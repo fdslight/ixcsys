32891f36a90abe6ab94916fa75f5b41ec377fa30
fdslight <freekai@outlook.com>
2020-10-21 05:17:29 +0800

    ...

--- ixc_syscore/router/pylib/ipcp.py
+++ ixc_syscore/router/pylib/ipcp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import random
+import random, socket
 
 import ixc_syscore.router.pylib.ncp as ncp
 import ixc_syscore.router.pylib.lcp as lcp
+
+import ixc_syscore.router.pylib.router as router
 
 import ixc_syslib.pylib.logging as logging
 
@@ -38,11 +40,10 @@ class IPCP(ncp.NCP):
                 if self.debug: print("Wrong length field value for IPCP")
                 break
             idx += 2
-            length = length - 2
-            e = idx + length
+            e = idx + length - 2
             opt_data = cfg_data[idx:e]
             idx = e
-            if len(opt_data) != length:
+            if len(opt_data) != length - 2:
                 results = []
                 if self.debug: print("Wrong IPCP option length field value")
                 break
@@ -57,6 +58,7 @@ class IPCP(ncp.NCP):
                 return
 
             if _type == 3 and length != 6:
+                print(length, "--")
                 results = []
                 if self.debug: print("Wrong data length for IPCP type %d" % _type)
                 return
@@ -64,6 +66,11 @@ class IPCP(ncp.NCP):
             results.append((_type, opt_data,))
 
         return results
+
+    def handle_cfg_request(self, _id: int, byte_data: bytes):
+        """重写这个方法
+        """
+        self.send_ncp_packet(0x8021, lcp.CFG_ACK, _id, byte_data)
 
     def send_ncp_ipaddr_request(self):
         """发送NCP的IP地址请求
@@ -84,7 +91,9 @@ class IPCP(ncp.NCP):
                 return
             if _type == 3:
                 self.__my_ipaddr = value
+                if self.debug: print("PPPoE My IP address:%s" % socket.inet_ntop(socket.AF_INET, value))
                 self.__ipaddr_ok = True
+                self.pppoe.runtime.router.netif_set_ip(router.IXC_NETIF_LAN, self.__my_ipaddr, 32, False)
                 break
             ''''''
         return
@@ -122,3 +131,9 @@ class IPCP(ncp.NCP):
         """重写这个方法
         """
         pass
+
+    def start(self):
+        self.send_ncp_ipaddr_request()
+
+    def get_ipaddr(self):
+        return socket.inet_ntop(socket.AF_INET, self.__my_ipaddr)
