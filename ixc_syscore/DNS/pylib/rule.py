16aa63a7a4e7d649ddd6a2e75864539fc26d8068
linux <freekai@outlook.com>
2020-11-08 01:28:52 +0800

    ...

--- ixc_syscore/DNS/pylib/rule.py
+++ ixc_syscore/DNS/pylib/rule.py
@@ -8,28 +8,90 @@
 
 
 class matcher(object):
+    __rule_tree = None
     __rules = None
 
     def __init__(self):
+        self.__rule_tree = {}
         self.__rules = {}
 
-    def match(self, domain: str):
-        """匹配域名
+    def match(self, host: str):
+        """匹配主机
         """
-        pass
+        _list = host.split(".")
+        _list.reverse()
 
-    def add_rule(self, rule: str, action: int):
+    def add_rule(self, rule: str, action):
         """加入规则
         :param rule,规则
         :param action
+        :return Boolean,添加成功那么返回True,不存在则返回False
         """
-        pass
+        if rule in self.__rules: return False
+        _list = rule.split(".")
+        _list.reverse()
+        o = self.__rule_tree
+
+        for x in _list:
+            # 新建的引用计数为0
+            if x not in o: o[x] = {"refcnt": 0, "action": None}
+            o = o[x]
+            o["refcnt"] += 1
+        o["action"] = action
+        self.__rules[rule] = None
+        return True
 
     def del_rule(self, rule: str):
         """删除规则
+        :return Boolean,删除成功那么返回True,不存在则返回False
         """
-        pass
+        if rule not in self.__rules: return False
+
+        _list = rule.split(".")
+        _list.reverse()
+
+        o = self.__rule_tree
+        is_found = True
+
+        for x in _list:
+            if x not in o:
+                is_found = False
+                break
+            o = o[x]
+        if not is_found: return
+
+        o = self.__rule_tree
+
+        for x in _list:
+            t = o
+            o = o[x]
+            o["refcnt"] -= 1
+            if o["refcnt"] == 0:
+                del t[x]
+                break
+            ''''''
+        del self.__rules[rule]
+        return True
+
+    @property
+    def rule_tree(self):
+        return self.__rule_tree
 
     @property
     def rules(self):
-        return self.__rules
+        rules = []
+        for x in self.__rules: rules.append(x)
+        return rules
+
+    def exists(self, rule: str):
+        """检查规则是否存在
+        """
+        return rule in self.__rules
+
+
+cls = matcher()
+cls.add_rule("x|y|z.google.com", "xxx")
+cls.add_rule("chrome.google.com", "this is action")
+# print(cls.rules)
+# print(cls.rule_tree)
+print(cls.match("chrome.google.com"))
