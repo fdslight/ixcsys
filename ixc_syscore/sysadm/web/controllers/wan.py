e6ccd2e8719307e73ec9e7c337b913af02c290e2
linux <freekai@outlook.com>
2020-11-21 22:41:57 +0800

    ...

--- ixc_syscore/sysadm/web/controllers/wan.py
+++ ixc_syscore/sysadm/web/controllers/wan.py
@@ -9,5 +9,15 @@ class controller(base_controller.BaseController):
         self.request.set_allow_methods(["GET", "POST"])
         return True
 
-    def handle(self):
+    def handle_get(self):
+        self.finish_with_json({"is_error": False, "message": {}})
+
+    def handle_post(self):
         self.finish_with_json({})
+
+    def handle(self):
+        method = self.request.environ["REQUEST_METHOD"]
+        if method == "GET":
+            self.handle_get()
+        else:
+            self.handle_post()
