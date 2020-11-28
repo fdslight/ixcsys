273d4bca4a8a0241197e9637841df6c7e5f77a41
linux <freekai@outlook.com>
2020-11-28 23:49:02 +0800

    ...

--- ixc_main.py
+++ ixc_main.py
@@ -13,13 +13,14 @@ must_services = [
 
 def set_pub_env():
     conf_dir = "%s/ixc_configs" % sys_dir
-    app_dir = "%s/ixc_apps"
+    app_dir = "%s/ixc_apps" % sys_dir
     sys_py_interpreter = sys.executable
 
     os.putenv("IXC_SYS_DIR", sys_dir)
     os.putenv("IXC_CONF_DIR", conf_dir)
     os.putenv("IXC_APP_DIR", app_dir)
     os.putenv("IXC_PYTHON", sys_py_interpreter)
+    os.putenv("IXC_SYSROOT", sys_dir)
 
     if not os.path.isdir("/tmp/ixcsys"): os.mkdir("/tmp/ixcsys")
 
--- ixc_syscore/sysadm/web/ui_widgets/applist.py
+++ ixc_syscore/sysadm/web/ui_widgets/applist.py
@@ -3,6 +3,5 @@ import ixc_syslib.web.ui_widget as ui_widget
 
 
 class widget(ui_widget.widget):
-
     def handle(self, *args, **kwargs):
-        return True, "applist.html", {"apps": []}
+        return True, "applist.html", {"apps": self.request_handler.get_all_apps()}
