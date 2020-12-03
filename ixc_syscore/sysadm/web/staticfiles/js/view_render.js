fb1ca5877608e5e03bd6ca56d70d676ebaa3d26f
linux <freekai@outlook.com>
2020-12-04 00:05:53 +0800

    ...

--- ixc_syscore/sysadm/web/staticfiles/js/view_render.js
+++ ixc_syscore/sysadm/web/staticfiles/js/view_render.js
@@ -3,32 +3,8 @@
  <p class="ixc-view_render"></p>
  */
 (function ($) {
-    function is_json(s) {
-        try {
-            JSON.parse(s);
-        } catch (e) {
-            return false;
-        }
-        return true;
-    }
 
-    function render_atrribute(selector, cls_prefix, name, v) {
-        /// 对标签属性进行渲染
-        let found = selector.find("*");
-        for (let n = 0; n < found.length; n++) {
-            let o = found[n];
-
-            for (let attr_name of o.getAttributeNames()) {
-                let attr_val = $(o).attr(attr_name);
-                let match_str = "{{" + cls_prefix + name + "}}";
-
-                attr_val = attr_val.replace(match_str, v);
-                $(o).attr(attr_name, attr_val);
-            }
-        }
-    }
-
-    function render_text(selector, cls_prefix, name, v) {
+    function render(cls_prefix, name, v) {
         /// 渲染标签内的html文本
         let found = selector.find("*");
         for (let n = 0; n < found.length; n++) {
@@ -50,8 +26,6 @@
         for (let name in json_data) {
             let v = json_data[name];
 
-            render_atrribute(selector, cls_prefix, name, v);
-            render_text(selector, cls_prefix, name, v);
         }
     };
 })(jQuery);
