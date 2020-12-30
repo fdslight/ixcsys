24409eaa0794b7db9bd0c417d6903e7636a49bfa
linux <freekai@outlook.com>
2020-12-30 19:19:41 +0800

    ...

--- test.py
+++ test.py
@@ -2,8 +2,6 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket()
 s.connect(("8.8.8.8", 8800))
-s.send(b"test UDP protocol")
-print(s.recvfrom(4096))
 s.close()
