625af20b507531e8fbf844267b65467d3403d66a
linux <freekai@outlook.com>
2021-01-16 23:39:22 +0800

    ...

--- ixc_syscore/proxy_helper/src/tcp_timer.h
+++ ixc_syscore/proxy_helper/src/tcp_timer.h
@@ -4,6 +4,7 @@
 #include<sys/types.h>
 struct tcp_timer_node;
 typedef int (*tcp_timer_cb)(void *session);
+typedef unsigned long long tcp_time_t;
 
 /// TCP timer节点
 struct tcp_timer_node{
@@ -14,14 +15,22 @@ struct tcp_timer_node{
     // 指向的TCP会话
     void *session;
     // 更新时间,单位是毫秒
-    unsigned long long up_time;
+    tcp_time_t up_time;
     // 超时时间,单位是毫秒
-    unsigned long long timeout;
+    tcp_time_t timeout;
     // 是否有效,如果为0表示该会话已经删除
     int is_valid;
 };
 
 struct tcp_timer{
+    struct tcp_timer_node *head;
+    // 更新时间
+    time_t up_time;
 };
+
+int tcp_timer_init(void);
+void tcp_timer_uninit(void);
+
+struct tcp_timer_node *tcp_timer_add(void);
 
 #endif
