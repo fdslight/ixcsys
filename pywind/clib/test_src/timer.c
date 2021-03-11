15fdf91f17797d61b2570870054ef08754c74d8a
linux <freekai@outlook.com>
2021-03-11 20:49:53 +0800

    ...

--- pywind/clib/test_src/timer.c
+++ pywind/clib/test_src/timer.c
@@ -6,23 +6,29 @@
 #include "../debug.h"
 
  
-
+   struct time_wheel wheel;
 static void timeout_fn(void *data)
 {
+    struct time_data *x;
+
+    x=time_wheel_add(&wheel,data,2);
+
     DBG("hello,world\r\n");
 }
 
 int main(int argc,char *argv[])
 {
-    struct time_wheel wheel;
-    int rs=time_wheel_new(&wheel,60,1,timeout_fn,16);
+ 
+    int rs=time_wheel_new(&wheel,60,2,timeout_fn,16);
     struct time_data *tdata=time_wheel_add(&wheel,NULL,1);
 
-    sleep(1);
+    sleep(2);
 
-    //printf("%d\r\n",rs);
+    printf("%d\r\n",rs);
 
     time_wheel_handle(&wheel);
+
+    sleep(2);
     time_wheel_handle(&wheel);
 
 
