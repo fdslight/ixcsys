bf2e30027ecf59b55e95b407927668cc28b47a75
mk <freekai@outlook.com>
2021-04-04 13:23:14 +0800

    ...

--- ixc_syscore/router/_ixc_router_helper.py
+++ ixc_syscore/router/_ixc_router_helper.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 import pickle
 
+import router
+
+import ixc_syslib.pylib.logging as logging
+import ixc_syscore.router.pylib.pppoe as pppoe
+
+import pywind.lib.netutils as netutils
+
 
 class rpc(object):
     __fn_objects = None
 
-    def __init__(self):
+    def __init__(self, _helper):
         self.__fn_objects = {
             "netif_set_ip": None,
             "netif_get_ip": None,
@@ -29,8 +36,39 @@ class rpc(object):
 
 
 class helper(object):
+    __router = None
+    __debug = None
+
+    __WAN_BR_NAME = None
+    __LAN_BR_NAME = None
+
+    __LAN_NAME = None
+    __WAN_NAME = None
+
+    __pppoe = None
+    __pppoe_enable = None
+    __pppoe_user = None
+    __pppoe_passwd = None
+
     def __init__(self):
+        self.__router = router.router()
+
+    @property
+    def router(self):
+        return self.__router
+
+    def pppoe_session_handle(self, protocol: int, byte_data: bytes):
+        """处理来自于C语言的PPPoE绘画数据
+        """
         pass
+
+    @property
+    def pppoe_user(self):
+        return self.__pppoe_user
+
+    @property
+    def pppoe_passwd(self):
+        return self.__pppoe_passwd
 
     def rpc_fn_call(self, name: str, arg_data: bytes):
         """C程序调佣此函数执行RPC
--- ixc_syscore/router/src/router.h
+++ ixc_syscore/router/src/router.h
@@ -16,14 +16,6 @@
 
 #include<sys/types.h>
 
-/// 发送数据到Python
-// if_type 表示接口,该值来自于netif.h
-// ipproto表示IP协议号,如果是链路层协议此参数为0
-// flags 表示额外的参数补充
-int ixc_router_send(unsigned char if_type,unsigned char ipproto,unsigned char flags,void *buf,size_t size);
-
-/// 写入事件告知
-int ixc_router_write_ev_tell(int fd,int flags);
 /// 发送PPPoE数据包到Python
 int ixc_router_pppoe_session_send(unsigned short protocol,unsigned short length,void *data);
 /// 通知函数
