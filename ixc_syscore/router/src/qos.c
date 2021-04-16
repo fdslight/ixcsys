9752cf43de8192e693edcd9f9b2ad57ce11c798f
kaimei <freekai@outlook.com>
2021-04-17 01:18:18 +0800

    ...

--- ixc_syscore/router/src/qos.c
+++ ixc_syscore/router/src/qos.c
@@ -20,22 +20,22 @@ static void ixc_qos_sysloop_cb(struct sysloop *lp)
     ixc_qos_pop();
 }
 
-inline static int ixc_qos_calc_slot(unsigned char a, unsigned char b, unsigned short _id)
+inline static int ixc_qos_calc_slot(unsigned char a, unsigned char b, unsigned char c,unsigned char d)
 {
-    unsigned int v= (a << 24) | (b<<16) | _id;
+    unsigned int v= (a << 24) | (b<<16) | (c<<8) | d;
     int slot_num= v % IXC_QOS_SLOT_NUM;
 
     return slot_num;
 }
 
-static void ixc_qos_put(struct ixc_mbuf *m,unsigned c,unsigned char ipproto)
+static void ixc_qos_put(struct ixc_mbuf *m,unsigned char a,unsigned char b,unsigned char c,unsigned char d)
 {
     unsigned short id=0;
     int slot_no;
     struct ixc_qos_slot *slot_obj;
 
     m->next=NULL;
-    slot_no=ixc_qos_calc_slot(c,ipproto,id);
+    slot_no=ixc_qos_calc_slot(a,b,c,d);
     slot_obj=ixc_qos.slot_objs[slot_no];
 
     if(!slot_obj->is_used){
@@ -56,14 +56,14 @@ static void ixc_qos_add_for_ip(struct ixc_mbuf *m)
 {
     struct netutil_iphdr *iphdr = (struct netutil_iphdr *)(m->data + m->offset);
 
-    ixc_qos_put(m,iphdr->dst_addr[3],iphdr->protocol);
+    ixc_qos_put(m,iphdr->src_addr[3],iphdr->dst_addr[1],iphdr->dst_addr[2],iphdr->dst_addr[3]);
 }
 
 static void ixc_qos_add_for_ipv6(struct ixc_mbuf *m)
 {
     struct netutil_ip6hdr *header=(struct netutil_ip6hdr *)(m->data+m->offset);
 
-    ixc_qos_put(m,header->dst_addr[15],header->next_header);
+    ixc_qos_put(m,header->src_addr[15],header->dst_addr[13],header->dst_addr[14],header->dst_addr[15]);
 }
 
 int ixc_qos_init(void)
