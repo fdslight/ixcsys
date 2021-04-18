d235f4f34c400500533b1cf584e30374413430e8
kaimei <freekai@outlook.com>
2021-04-18 18:30:03 +0800

    ...

--- ixc_syscore/DHCP/pylib/dhcp_server.py
+++ ixc_syscore/DHCP/pylib/dhcp_server.py
@@ -250,9 +250,6 @@ class dhcp_server(object):
         self.__alloc.bind_ipaddr(s_client_hwaddr, o["ip"])
         self.__dhcp_builder.set_boot(self.__hostname, self.__boot_file)
 
-        vendor = self.get_dhcp_opt_value(opts,60)
-        if vendor:
-            resp_opts.append((60, vendor))
         self.dhcp_msg_send(resp_opts)
 
     def handle_dhcp_decline(self, opts: list):
