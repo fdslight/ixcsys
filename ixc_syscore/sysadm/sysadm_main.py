5f7a809b63b63152b309a81cd077dcec97f4f69a
kaimei <freekai@outlook.com>
2021-12-07 18:44:01 +0800

    ...

--- ixc_syscore/sysadm/sysadm_main.py
+++ ixc_syscore/sysadm/sysadm_main.py
@@ -418,7 +418,7 @@ class service(dispatcher.dispatcher):
     def diskless_os_cfg_get(self, hwaddr: str):
         """获取无盘的操作系统配置
         """
-        if hwaddr in self.__diskless_cfg_macs:
+        if hwaddr not in self.__diskless_cfg_macs:
             hwaddr = "00:00:00:00:00:00"
 
         return self.__diskless_cfg_macs.get(hwaddr, {})
