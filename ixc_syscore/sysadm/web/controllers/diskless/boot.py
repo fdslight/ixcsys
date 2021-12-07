4fcf3f8786bcf6c6d35cb5ed8483f43660953c67
kaimei <freekai@outlook.com>
2021-12-07 17:47:09 +0800

    ...

--- ixc_syscore/sysadm/web/controllers/diskless/boot.py
+++ ixc_syscore/sysadm/web/controllers/diskless/boot.py
@@ -25,8 +25,13 @@ class controller(base_controller.BaseController):
         os_info = self.sysadm.diskless_os_cfg_get(hwaddr)
 
         if not os_info:
-            self.send_exit(reason="not found config for %s" % hwaddr)
-            return
+            # 查找默认MAC是否存在
+            hwaddr = "00:00:00:00:00:00"
+            os_info = self.sysadm.diskless_os_cfg_get(hwaddr)
+
+            if not os_info:
+                self.send_exit(reason="not found config for %s" % hwaddr)
+                return
 
         script_path = os_info["script-path"]
 
