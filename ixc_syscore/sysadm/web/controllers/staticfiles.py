f4c56c9ecc01ed992273d27adafc0dffe64e4b64
freek <freekai@outlook.com>
2022-04-11 19:39:59 +0800

    ...

--- ixc_syscore/sysadm/web/controllers/staticfiles.py
+++ ixc_syscore/sysadm/web/controllers/staticfiles.py
@@ -5,4 +5,5 @@ import ixc_syslib.web.controllers.staticfiles as staticfiles
 
 class controller(staticfiles.controller):
     def staticfile_init(self):
+        self.set_mime("map", "application/json;charset=utf-8")
         self.set_no_cache()
