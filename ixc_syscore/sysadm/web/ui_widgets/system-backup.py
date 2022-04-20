82e721c7638d1ef2a1a83e08e811d912fbd6bc9e
freek <freekai@outlook.com>
2022-04-20 22:25:19 +0800

    ...

--- ixc_syscore/sysadm/web/ui_widgets/system-backup.py
+++ ixc_syscore/sysadm/web/ui_widgets/system-backup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+import os
 import ixc_syslib.web.ui_widget as ui_widget
+
 
 class widget(ui_widget.widget):
     def handle(self, *args, **kwargs):
-        return True, "system-backup.html", {"backup-files":[]}
+        d = "/var/log"
+        files = []
+        for x in os.listdir(d):
+            p = x.find("ixcsys_config_backup.")
+            if p != 0: continue
+            path = "%s/%s" % (d, x,)
+            if not os.path.isfile(path): continue
+            files.append(x)
+
+        return True, "system-backup.html", {"backup-files": files}
