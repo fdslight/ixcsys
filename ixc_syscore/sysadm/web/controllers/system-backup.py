0ea5202ef58cf7d66dabd17b92fe53837ce4c8a2
freek <freekai@outlook.com>
2022-04-20 22:49:50 +0800

    ...

--- ixc_syscore/sysadm/web/controllers/system-backup.py
+++ ixc_syscore/sysadm/web/controllers/system-backup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os, time
 
 import ixc_syscore.sysadm.web.controllers.controller as base_controller
 
@@ -14,5 +15,42 @@ class controller(base_controller.BaseController):
     def sysadm(self):
         return global_vars["ixcsys.sysadm"]
 
+    def do_backup(self):
+        t = time.strftime("%Y-%m-%d_%H%M%S")
+        fpath = "/var/log/ixcsys_config_backup.%s.tar.gz" % t
+        os.system("tar czf %s /opt/ixcsys/ixc_configs" % fpath)
+
+        self.json_resp(False, "")
+
     def handle(self):
+        action = self.request.get_argument("do", is_seq=False, is_qs=False)
+        if action not in ("delete", "recovery", "backup",):
+            self.json_resp(True, "错误的请求参数")
+            return
+
+        if action == "backup":
+            self.do_backup()
+            return
+
+        file_name = self.request.get_argument("file", is_seq=False, is_qs=False)
+        if not file_name:
+            self.json_resp(True, "非法的文件名")
+            return
+
+        # 检查文件是否合法,避免提交参数随意操作不允许的文件
+        p = file_name.find("ixcsys_config_backup.")
+        if p != 0:
+            self.json_resp(True, "非法的文件名")
+            return
+
+        fpath = "/var/log" % file_name
+        if not os.path.isfile(fpath):
+            self.json_resp(True, "不存在文件%s" % file_name)
+            return
+
+        if action == "delete":
+            os.remove(fpath)
+        else:
+            os.system("tar xzf %s -C /opt/ixcsys" % fpath)
+
         self.json_resp(False, "")
