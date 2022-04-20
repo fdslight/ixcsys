1243e7966d634042ab5c99d7de3610b4a79346d6
freek <freekai@outlook.com>
2022-04-20 22:08:24 +0800

    ...

---
+++ ixc_syscore/sysadm/web/ui_widgets/system-backup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import ixc_syslib.web.ui_widget as ui_widget
+
+class widget(ui_widget.widget):
+    def handle(self, *args, **kwargs):
+        return True, "system-backup.html", {"backup-files":[]}
