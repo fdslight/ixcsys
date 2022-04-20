3b72578e93535f584afc7090a1e06825982c4dd6
freek <freekai@outlook.com>
2022-04-20 19:00:28 +0800

    ...

---
+++ ixc_syscore/home_vpn/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+${IXC_PYTHON} ${IXC_MYAPP_DIR}/home_vpn_main.py start
---
+++ ixc_syscore/home_vpn/web/controllers/RPC/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pywind.global_vars import global_vars
+
+import ixc_syslib.web.controllers.rpc_controller as rpc
+
+
+class controller(rpc.controller):
+    __runtime = None
+
+    def rpc_init(self):
+        self.__runtime = global_vars["ixcsys.home_vpn"]
+
+        self.fobjs = {
+        }
