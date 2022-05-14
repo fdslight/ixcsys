b6d18b0fadea8a83a78438e5ca2a60b94fb487e6
freek <freekai@outlook.com>
2022-05-15 01:22:36 +0800

    ...

--- ixc_syscore/proxy/make.py
+++ ixc_syscore/proxy/make.py
@@ -25,4 +25,5 @@ def install(root_dir, install_root_dir: str, name: str):
     path = "%s/%s" % (install_root_dir, name)
     os.system("mkdir -p %s/%s" % (install_root_dir, name,))
     os.system("cp -r %s/%s/* %s" % (root_dir, name, path))
+    os.system("rm %s/pylib/*.c" % path)
     os.system("rm %s/make.py" % path)
