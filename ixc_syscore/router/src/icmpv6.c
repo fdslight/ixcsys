3f7eb075e3e9ffae482951ad04a2dd37ff898414
freek <freekai@outlook.com>
2023-02-02 19:54:36 +0800

    ...

--- ixc_syscore/router/src/icmpv6.c
+++ ixc_syscore/router/src/icmpv6.c
@@ -129,7 +129,7 @@ static void ixc_icmpv6_handle_rs(struct ixc_mbuf *m,struct netutil_ip6hdr *iphdr
 /// 处理路由宣告报文
 static void ixc_icmpv6_handle_ra(struct ixc_mbuf *m,struct netutil_ip6hdr *iphdr,unsigned char icmp_code)
 {
-    struct ixc_netif *netif=m->netif,*if_lan;
+    struct ixc_netif *netif=m->netif;
     //struct ixc_icmpv6_ra_header *ra_header;
     struct ixc_icmpv6_opt_prefix_info *opt_prefix;
 
@@ -137,7 +137,7 @@ static void ixc_icmpv6_handle_ra(struct ixc_mbuf *m,struct netutil_ip6hdr *iphdr
     unsigned char type,length;
     int is_err=0,A=0;
     unsigned char gw_hwaddr[6],byte,slaac_addr[16];
-    unsigned char unspec_addr[]=IXC_IP6ADDR_UNSPEC;
+    //unsigned char unspec_addr[]=IXC_IP6ADDR_UNSPEC;
     unsigned int mtu;
 
     if(m->tail-m->offset!=64){
