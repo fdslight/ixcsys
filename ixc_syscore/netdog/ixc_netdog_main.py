d4c39660f67f679b6bc8f561d9cacd195258df7e
freek <freekai@outlook.com>
2023-02-19 17:08:59 +0800

    ...

--- ixc_syscore/netdog/ixc_netdog_main.py
+++ ixc_syscore/netdog/ixc_netdog_main.py
@@ -85,6 +85,7 @@ class service(dispatcher.dispatcher):
         time.sleep(3)
         # 启动网络分析器后,进行数据转发配置,用以监控局域网流量
         mon_key, mon_port = libsys_msg.get_pkt_mon_port()
+        print(mon_key,mon_port)
         # 首先关闭流量拷贝
         RPCClient.fn_call("router", "/config", "traffic_cpy_enable", False)
         RPCClient.fn_call("router", "/config", "unset_fwd_port", self.__consts['IXC_FLAG_TRAFFIC_COPY'])
