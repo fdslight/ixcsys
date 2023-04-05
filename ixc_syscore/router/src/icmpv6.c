99704cb1730437e8a3ac8aa33cee345a94a8c030
freek <freekai@outlook.com>
2023-04-06 01:55:18 +0800

    ...

--- ixc_syscore/router/src/icmpv6.c
+++ ixc_syscore/router/src/icmpv6.c
@@ -684,7 +684,7 @@ void ixc_icmpv6_filter_and_modify(struct ixc_mbuf *m)
     icmp_header->checksum=0;
     icmp_header->checksum=csum_calc((unsigned short *)(buf),offset);
 
-    IPv6_HEADER_SET(ip6_header,0,0,offset-40,58,64,ps_header->src_addr,ps_header->dst_addr);
+    IPv6_HEADER_SET(ip6_header,0,0,offset-40,58,255,ps_header->src_addr,ps_header->dst_addr);
 
     memcpy(m->data+m->offset+40,buf+40,offset-40);
 
