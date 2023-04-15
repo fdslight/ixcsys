77b6292cab876f3b29037951511255f954f86912
freek <freekai@outlook.com>
2023-04-15 10:49:19 +0800

    ...

--- ixc_syscore/simpleNAS/iscsid/session.h
+++
@@ -1,9 +0,0 @@
-#ifndef IXC_ISCSI_SESSION_H
-#define IXC_ISCSI_SESSION_H
-
-#include<arpa/inet.h>
-
-int ixc_iscsi_session_create(int fd,void *sockaddr,socklen_t addrlen,int is_ipv6);
-void ixc_iscsi_session_delete(void);
-
-#endif
