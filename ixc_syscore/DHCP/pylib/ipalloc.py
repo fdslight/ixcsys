afa19c920c097eae03ea0a8ac5350b75af30cf92
freek <freekai@outlook.com>
2023-04-15 11:28:46 +0800

    ...

--- ixc_syscore/DHCP/pylib/ipalloc.py
+++ ixc_syscore/DHCP/pylib/ipalloc.py
@@ -71,16 +71,14 @@ class alloc(object):
         :param ipaddr,IP地址
         """
         # 检查有无存在冲突
-        """
         flags = False
         for tmp_hwaddr, tmp_ipaddr in self.__bind.items():
             if tmp_ipaddr == ipaddr:
                 flags = True
                 break
             ''''''
-        """
-        #if not flags: self.__bind[hwaddr] = ipaddr
-        self.__bind[hwaddr] = ipaddr
+
+        if not flags: self.__bind[hwaddr] = ipaddr
 
     def unbind_ipaddr(self, hwaddr: str):
         if hwaddr not in self.__bind: return
