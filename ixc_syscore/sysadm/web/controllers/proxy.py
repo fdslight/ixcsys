9a64f575529537e20b65e3566787a53b7be13895
freek <freekai@outlook.com>
2023-08-10 21:13:26 +0800

    ...

--- ixc_syscore/sysadm/web/controllers/proxy.py
+++ ixc_syscore/sysadm/web/controllers/proxy.py
@@ -163,9 +163,12 @@ class controller(base_controller.BaseController):
         else:
             kv_map["tunnel_over_https"]["strict_https"] = "0"
 
-        if kv_map["tunnel_over_https"]["ciphers"].strip() == ",":
+        ciphers = kv_map["tunnel_over_https"]["ciphers"].strip()
+        if ciphers == ",":
             self.json_resp(True, "错误的TLS加密算法")
             return
+        if not ciphers:
+            kv_map["tunnel_over_https"]["ciphers"] = "NULL"
 
         if not kv_map["tunnel_over_https"]["url"]:
             self.json_resp(True, "HTTPS隧道的url不能为空")
