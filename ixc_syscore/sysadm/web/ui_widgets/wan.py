9067fc5480c43c04d39c664632568902ffd37190
freek <freekai@outlook.com>
2023-09-24 13:33:18 +0800

    ...

--- ixc_syscore/sysadm/web/ui_widgets/wan.py
+++ ixc_syscore/sysadm/web/ui_widgets/wan.py
@@ -8,13 +8,14 @@ class widget(ui_widget.widget):
     def handle(self, *args, **kwargs):
         _type = self.get_argument("type", default="pppoe", is_qs=True, is_seq=False)
 
-        if _type not in ("pppoe", "dhcp", "static-ip",): _type = "pppoe"
+        if _type not in ("dhcp", "static-ip",): _type = "dhcp"
         kwargs = {}
         configs = RPC.fn_call("router", "/config", "wan_config_get")
 
         if _type == "pppoe":
             kwargs = configs["pppoe"]
             kwargs["heartbeat"] = bool(int(kwargs["heartbeat"]))
+
         if _type == "static-ip":
             kwargs = configs["ipv4"]
 
