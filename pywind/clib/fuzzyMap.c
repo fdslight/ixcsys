2a7490c620ad816dfa8f4b1853c822a45b7a39b9
freek <freekai@outlook.com>
2023-10-03 18:49:31 +0800

    ...

--- pywind/clib/fuzzyMap.c
+++ pywind/clib/fuzzyMap.c
@@ -292,11 +292,10 @@ void *fuzzyMap_find(struct fuzzyMap *m,const char *key,char *is_found,unsigned i
 		tmp_node=node;
 		node=node->next_nodes[v];
 		if(NULL==node) {
-			for(int i=0;i<0xff;i++){
-				node=tmp_node->next_nodes[i];
-				if(NULL!=node) break;
-			}
+			v=0;
+			node=tmp_node->next_nodes[v];
 		}
+
 		if(0!=v) count+=1;
 		if(NULL==node) break;
 	}
