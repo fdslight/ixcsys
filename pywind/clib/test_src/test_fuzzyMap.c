03a7f419b8c97f3b6f083bda54d02252cdc5db3d
freek <freekai@outlook.com>
2023-10-03 18:13:37 +0800

    ...

--- pywind/clib/test_src/test_fuzzyMap.c
+++ pywind/clib/test_src/test_fuzzyMap.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 
-#include "../map.h"
+#include "../fuzzyMap.h"
 
 void cb(void *x)
 {
@@ -11,21 +11,21 @@ void cb(void *x)
 
 int main(int argc,char *argv[])
 {
-    struct map *m;
-    int rs=map_new(&m,4);
+    struct fuzzyMap *m;
+    int rs=fuzzyMap_new(&m,4);
     int x=1000,*z;
     char is_found;
 
-    rs=map_pre_alloc(m,256);
-    rs=map_add(m,"hell",&x);
-    rs=map_add(m,"herd",&x);
+    rs=fuzzyMap_pre_alloc(m,256);
+    rs=fuzzyMap_add(m,"hell",&x);
+    rs=fuzzyMap_add(m,"herd",&x);
     
-    z=map_find(m,"hell",&is_found);
-    map_del(m,"hell",NULL);
-    map_each(m,cb);
+    z=fuzzyMap_find(m,"hell",&is_found);
+    fuzzyMap_del(m,"hell",NULL);
+    fuzzyMap_each(m,cb);
     
     
-    map_del(m,"herd",NULL);
+    fuzzyMap_del(m,"herd",NULL);
     
     printf("%d %d %d\r\n",rs,is_found,*z);
 
