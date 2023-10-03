475c51184d01cdfba87b8121b8bf1c81f2dad9a0
freek <freekai@outlook.com>
2023-10-03 18:33:44 +0800

    ...

--- pywind/clib/test_src/test_fuzzyMap.c
+++ pywind/clib/test_src/test_fuzzyMap.c
@@ -13,13 +13,13 @@ int main(int argc,char *argv[])
 {
     struct fuzzyMap *m;
     int rs=fuzzyMap_new(&m,4);
-    int x=1000,*z;
+    int x=1000,y=2000,*z;
     unsigned int match_cnt;
     char is_found;
 
     rs=fuzzyMap_pre_alloc(m,256);
     rs=fuzzyMap_add(m,"hell",&x);
-    rs=fuzzyMap_add(m,"herd",&x);
+    rs=fuzzyMap_add(m,"herd",&y);
     
     z=fuzzyMap_find(m,"hel\0",&is_found,&match_cnt);
 
