5bb5062eccb08fee6f6ce7f495832b668672c1eb
freek <freekai@outlook.com>
2023-10-22 12:14:37 +0800

    ...

--- no_ads_generate.py
+++ no_ads_generate.py
@@ -13,8 +13,8 @@ EXTRA_ADDS = [
 EXTRA_DROPS = [
     "google", "twitter", "facebook", "microsoft", "bing",
     ".qy.net", "doubleclick.net", "doubleclick.com",
-    "sentry.io", "app-measurement", "cloudflare.com",".stripe.com",
-    ".tw",".philips.","cloudfront.net",
+    "sentry.io", "app-measurement", "cloudflare.com", ".stripe.com",
+    ".tw", ".philips.", ".cloudfront.net", ".amazonaws.com", ".amazon.", "americanexpress",
 ]
 
 
