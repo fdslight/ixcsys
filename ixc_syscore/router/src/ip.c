df748c94b604d46b83fd48b2c96ba0eaf4d60274
freek <freekai@outlook.com>
2023-12-03 12:15:41 +0800

    ...

--- ixc_syscore/router/src/ip.c
+++ ixc_syscore/router/src/ip.c
@@ -153,7 +153,7 @@ static void ixc_ip_handle_from_lan(struct ixc_mbuf *m,struct netutil_iphdr *iphd
     }
 
     // 如果开启非系统DNS请求丢弃,检查是否是系统DNS请求,如果不是那么丢弃数据包
-    if(ip_enable_no_system_dns_drop && ixc_ip_is_no_system_dns_req(m,header)){
+    if(ip_enable_no_system_dns_drop && ixc_ip_is_no_system_dns_req(m,iphdr)){
         ixc_mbuf_put(m);
         return;
     }
