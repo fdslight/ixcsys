052387f4af2c436165908e40ef37af5dc1313c9c
linux <freekai@outlook.com>
2024-02-10 11:09:37 +0800

    ...

--- pywind/cweb/src/cweb.c
+++ pywind/cweb/src/cweb.c
@@ -1,6 +1,29 @@
+#include<getopt.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
 
 #include "../../clib/debug.h"
 
+/// @发送停止web服务器信号
+/// @param path 
+static void send_stop_web_sig(const char *path)
+{
+    struct stat s_buf;
+    
+    if(access(path,F_OK)){
+        printf("ERROR:not found pid file %s\r\n",path);
+        return;
+    }
+
+    stat(path,&s_buf);
+
+    if(S_ISDIR(s_buf.st_mode)){
+        printf("ERROR:%s is a directory\r\n",path);
+        return;
+    }
+}
 
 int main(int argc,char *argv[])
 {
@@ -14,10 +37,15 @@ stop process-pid-file \
         return -1;
     }
 
-    
+    if(strcmp(argv[1],"start") && strcmp(argv[1],"stop")){
+        printf("%s\r\n",help_doc);
+        return -1;
+    }
 
-
-
+    if(!strcmp(argv[1],"stop")){
+        send_stop_web_sig(argv[2]);
+        return 0;
+    }
     
     return 0;
 }
