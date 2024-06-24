400075a0087a8058d0776f22dc6cdf7cff38400d
freek <freekai@outlook.com>
2024-06-24 21:45:47 +0800

    ...

---
+++ ixc_cron_updater.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# 自动更新路由器软件脚本
+import os, sys, time
+
+# 更新文件路径
+UPDATE_FILE = "/tmp/ixcsys_update.tar.gz"
+
+
+def do_update():
+    if not os.path.isfile(UPDATE_FILE):
+        return
+
+    cmd = "%s /opt/ixcsys/ixc_main.py restart" % sys.executable
+    os.system(cmd)
+
+
+def start(h, m):
+    while 1:
+        now_h = time.strftime("%H")
+        now_m = time.strftime("%M")
+
+        if now_h == h and now_m == m:
+            print("NOTIFY:start auto update")
+            do_update()
+
+        time.sleep(30)
+
+
+def main():
+    helper = """argument   HOUR:MINUTE"""
+    user = os.getenv("USER")
+    if user.lower() != "root":
+        print("ERROR:run this script must be root user")
+        return
+
+    if len(sys.argv) != 2:
+        print(helper)
+        return
+
+    _list = sys.argv[1].split(":")
+    if len(_list) != 2:
+        print("ERROR:wrong time format")
+        return
+
+    h = _list[0]
+    m = _list[1]
+
+    try:
+        h = int(h)
+        m = int(m)
+    except ValueError:
+        print("ERROR:wrong time format")
+        return
+
+    if h < 0 or h > 23:
+        print("ERROR:wrong time hour value format")
+        return
+
+    if m < 0 or m > 59:
+        print("ERROR:wrong time minute value format")
+        return
+
+    start(h, m)
+
+
+if __name__ == '__main__': main()
