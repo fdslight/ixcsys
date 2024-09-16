cc1d299c4ade6846809562d19128897cc71cb8b4
freek <freekai@outlook.com>
2024-09-16 17:44:56 +0800

    ...

--- ixc_syscore/sysadm/web/ui_widgets/edns.py
+++ ixc_syscore/sysadm/web/ui_widgets/edns.py
@@ -6,7 +6,6 @@ import ixc_syslib.pylib.RPCClient as RPC
 class widget(ui_widget.widget):
     def handle(self, *args, **kwargs):
         dns_servers = RPC.fn_call("secDNS", "/config", "dot_server_get")
-        print(dns_servers)
         uri = "edns.html"
 
         return True, uri, {"servers": dns_servers}
