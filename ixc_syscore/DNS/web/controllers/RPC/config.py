bba462e84a48aadf25be147851d87f7bf455c7d3
freek <freekai@outlook.com>
2024-09-16 20:20:02 +0800

    ...

--- ixc_syscore/DNS/web/controllers/RPC/config.py
+++ ixc_syscore/DNS/web/controllers/RPC/config.py
@@ -154,7 +154,7 @@ class controller(rpc.controller):
         if port < 1 or port > 65535:
             return 0, False
 
-        if len(bytes) != 4 or not isinstance(key, bytes):
+        if len(key) != 4 or not isinstance(key, bytes):
             return 0, False
 
         self.__runtime.set_sec_dns_forward(port, key)
