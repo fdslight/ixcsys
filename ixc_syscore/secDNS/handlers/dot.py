e1b13c3a19b18aba95d1014283b626663bb4beb0
freek <freekai@outlook.com>
2024-09-17 01:00:37 +0800

    ...

--- ixc_syscore/secDNS/handlers/dot.py
+++ ixc_syscore/secDNS/handlers/dot.py
@@ -169,7 +169,7 @@ class dot_client(tcp_handler.tcp_handler):
             self.add_evt_write(self.fileno)
         except ssl.SSLZeroReturnError:
             self.delete_handler(self.fileno)
-            #logging.print_error("SSL handshake fail %s" % self.__hostname)
+            # logging.print_error("SSL handshake fail %s" % self.__hostname)
         except:
             logging.print_info("SSL ERROR %s" % self.__hostname)
             self.delete_handler(self.fileno)
@@ -216,6 +216,8 @@ class dot_client(tcp_handler.tcp_handler):
         now = time.time()
         if now - self.__update_time >= self.__conn_timeout:
             self.delete_handler(self.fileno)
+            return
+        self.set_timeout(self.fileno, self.__LOOP_TIMEOUT)
 
     def send_to_server(self, message: bytes):
         length = len(message)
