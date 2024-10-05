be715cc1ad94a61ac7cbc988082334da1b55b9f4
freek <freekai@outlook.com>
2024-10-05 16:25:57 +0800

    ...

--- ixc_syscore/sysadm/web/ui_widgets/passthrough-device.py
+++ ixc_syscore/sysadm/web/ui_widgets/passthrough-device.py
@@ -7,6 +7,6 @@ class widget(ui_widget.widget):
     def handle(self, *args, **kwargs):
         router_configs = RPC.fn_call("router", "/config", "router_config_get")
         passthrough_devices = router_configs["passthrough"]
-        print(passthrough_devices)
         uri = "passthrough-device.html"
+
         return True, uri, {"devices": passthrough_devices}
