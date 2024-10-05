86495d65f1b4419bc6596d9624dc9b1f49569666
freek <freekai@outlook.com>
2024-10-05 15:56:09 +0800

    ...

--- ixc_syscore/sysadm/web/ui_widgets/passthrough-device.py
+++ ixc_syscore/sysadm/web/ui_widgets/passthrough-device.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import ixc_syslib.web.ui_widget as ui_widget
 import ixc_syslib.pylib.RPCClient as RPC
-from pywind.lib.crpc import RPCClient
 
 
 class widget(ui_widget.widget):
     def handle(self, *args, **kwargs):
-        router_configs = RPCClient.fn_call("router", "/config", "router_config_get")
+        router_configs = RPC.fn_call("router", "/config", "router_config_get")
         passthrough_devices = router_configs["passthrough"]
 
         uri = "passthrough-device.html"
