e93a48ac1f4ea03460d23c80126c0ad96107f0aa
freek <freekai@outlook.com>
2024-12-31 00:25:49 +0800

    ...

--- PASSClient/src/npfwd.c
+++ PASSClient/src/npfwd.c
@@ -34,7 +34,7 @@ static void ixc_npfwd_rx_data(int fd)
         }
 
         recv_size=recvfrom(fd,m->data+IXC_MBUF_BEGIN,IXC_MBUF_DATA_MAX_SIZE-IXC_MBUF_BEGIN,0,&from,&fromlen);
-        DBG_FLAGS;
+
         if(recv_size<0){
             ixc_mbuf_put(m);
             break;
