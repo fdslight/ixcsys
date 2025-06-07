e9b8203e26e9c90363a3deb6aa48e132ef881f14
freek <freekai@outlook.com>
2025-06-08 00:50:26 +0800

    ...

--- ixc_syscore/router/src/src_filter.c
+++ ixc_syscore/router/src/src_filter.c
@@ -104,7 +104,8 @@ int ixc_src_filter_add_hwaddr(const unsigned char *hwaddr)
 {
     char is_found;
     int rs;
-    void *data=map_find(src_filter.map,(char *)hwaddr,&is_found);
+    
+    map_find(src_filter.map,(char *)hwaddr,&is_found);
     if(is_found) return 1;
 
     rs=map_add(src_filter.map,(char *)hwaddr,NULL);
