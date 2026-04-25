22b9e0acdc5a83e102e348139442a188692b3505
linux <freekai@outlook.com>
2026-04-25 22:05:11 +0800

    ...

--- pywind/clib/test_src/test_map.c
+++ pywind/clib/test_src/test_map.c
@@ -16,16 +16,19 @@ int main(int argc,char *argv[])
     int x=1000,*z;
     char is_found;
 
+    int k1=2000;
+    int k2=3000;
+
     rs=map_pre_alloc(m,256);
-    rs=map_add(m,"hell",&x);
-    rs=map_add(m,"herd",&x);
+    rs=map_add(m,(char *)&k1,&x);
+    rs=map_add(m,(char *)&k2,&x);
     
-    z=map_find(m,"hell",&is_found);
-    map_del(m,"hell",NULL);
+    z=map_find(m,(char *)&k1,&is_found);
+    map_del(m,(char *)&k1,NULL);
     map_each(m,cb);
     
     
-    map_del(m,"herd",NULL);
+    map_del(m,(char *)&k2,NULL);
     
     printf("%d %d %d\r\n",rs,is_found,*z);
 
